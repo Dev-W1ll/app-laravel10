7b3644a6c2c4cbe5db2dd587b09f95b56dadb98e
Dev-W1ll <93744707+Dev-W1ll@users.noreply.github.com>
2024-02-17 18:34:37 -0300

    feat: scaffold authentication - starter kit Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Enums\SupportStatus;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Site\SiteController;
 use App\Http\Controllers\Admin\{SupportController};
 
@@ -16,17 +17,29 @@ use App\Http\Controllers\Admin\{SupportController};
 |
 */
 
-// Route::resource('/supports', SupportController::class);
-Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
-Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
-Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
-
 Route::get('/contato', [SiteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route::resource('/supports', SupportController::class);
+    Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+    Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+    Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+    Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+});
+
+require __DIR__ . '/auth.php';
