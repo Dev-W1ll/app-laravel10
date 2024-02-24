5307835e2602a4427f8d6afda6676bba3e099976
Dev-W1ll <93744707+Dev-W1ll@users.noreply.github.com>
2024-02-24 18:58:05 -0300

    refactor: create new module for replies of support

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Enums\SupportStatus;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Site\SiteController;
-use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\Admin\{ReplySupportController, SupportController};
 
 /*
 |--------------------------------------------------------------------------
@@ -32,12 +31,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('/supports/{id}/replies', [ReplySupportController::class, 'index'])->name('replies.index');
+
     // Route::resource('/supports', SupportController::class);
     Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
     Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
     Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
     Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
     Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
     Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 });
