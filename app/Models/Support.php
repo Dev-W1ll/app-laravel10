63912e7b388e9e335c9749b6d0a6a2e719ba0581
Dev-W1ll <93744707+Dev-W1ll@users.noreply.github.com>
2024-03-02 20:37:35 -0300

    feat: define relationships

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use App\Enums\SupportStatus;
+use App\Models\ReplySupport;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Support extends Model
 {
@@ -27,5 +31,15 @@ class Support extends Model
         return Attribute::make(
             set: fn(SupportStatus $status) => $status->name,
         );
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies(): HasMany
+    {
+        return $this->hasMany(ReplySupport::class);
     }
 }
