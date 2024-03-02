6e6b129bc8fb15577af7ba97217c9ef98b67c62a
Dev-W1ll <93744707+Dev-W1ll@users.noreply.github.com>
2024-03-02 20:33:27 -0300

    feat: create table and model replies support

---
+++ app/Models/ReplySupport.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Support;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ReplySupport extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'replies_support';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function support(): BelongsTo
+    {
+        return $this->belongsTo(Support::class);
+    }
+}
