5ad20240d03d7fe58f8e58532944ea9411a5a6e9
Dev-W1ll <93744707+Dev-W1ll@users.noreply.github.com>
2024-03-16 22:13:50 -0300

    feat: delete reply

--- app/Repositories/Contracts/ReplyRepositoryInterface.php
+++ app/Repositories/Contracts/ReplyRepositoryInterface.php
@@ -8,6 +8,6 @@ use stdClass;
 interface ReplyRepositoryInterface
 {
     public function getAllBySupportId(string $supportId): array;
-
     public function createNew(CreateReplyDTO $dto): stdClass;
+    public function delete(string $id): bool;
 }
