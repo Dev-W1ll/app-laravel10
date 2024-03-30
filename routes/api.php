0cc5dabcbd8958a60796d260af924b02886884e3
Dev-W1ll <93744707+Dev-W1ll@users.noreply.github.com>
2024-03-30 20:12:53 -0300

    feat: create route list all replies of support

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\ReplySupportApiController;
 use App\Http\Controllers\Api\SupportController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,13 +14,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
 Route::post('/login', [AuthController::class, 'auth']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/replies/{support_id}', [ReplySupportApiController::class, 'getRepliesFromSupport']);
+
     Route::apiResource('/supports', SupportController::class);
 });
 
