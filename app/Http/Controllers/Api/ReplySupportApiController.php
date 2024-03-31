02e69db2ab7ca7c4b9d49f3e31f66b1e4990cff6
Dev-W1ll <93744707+Dev-W1ll@users.noreply.github.com>
2024-03-30 23:22:53 -0300

    feat: delete reply

--- app/Http/Controllers/Api/ReplySupportApiController.php
+++ app/Http/Controllers/Api/ReplySupportApiController.php
@@ -39,4 +39,11 @@ class ReplySupportApiController extends Controller
             ->response()
             ->setStatusCode(Response::HTTP_CREATED);
     }
+
+    public function destroy(string $id)
+    {
+        $this->replyService->delete($id);
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
 }
